0ef5ea079ea8fb6adffa331caec6d71cd2cc0a2e
shakhawatsalam <shakhawatsalam726@gmail.com>
2023-09-01 07:31:17 +0600

    [User Service and Controller created]

---
+++ src/app/modules/user/user.controller.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from 'express';
+import httpStatus from 'http-status';
+import catchAsync from '../../../shared/catchAsync';
+import sendResponse from '../../../shared/sendResponse';
+import { UserService } from './user.service';
+
+const insertIntoDB = catchAsync(async (req: Request, res: Response) => {
+  const result = await UserService.insertIntoDB(req.body);
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'User Created Successfully',
+    data: result,
+  });
+});
+
+export const UserController = {
+  insertIntoDB,
+};
