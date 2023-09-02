da7243670c54d6c1d22d612c3898dc76d68efda5
shakhawatsalam <shakhawatsalam726@gmail.com>
2023-09-02 15:09:20 +0600

    [Category's Access Validation is Done]

--- src/app/modules/category/category.routes.ts
+++ src/app/modules/category/category.routes.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import { ENUM_USER_ROLE } from '../../../enums/user';
+import auth from '../../middlewares/auth';
 import validateRequest from '../../middlewares/validateRequest';
 import { CategoryController } from './category.controller';
 import { CategoryValidation } from './category.validation';
@@ -9,15 +11,21 @@ router.get('/', CategoryController.getAllBooksFromDB);
 router.get('/:id', CategoryController.getSingleBooksFromDB);
 router.post(
   '/create-category',
+  auth(ENUM_USER_ROLE.ADMIN),
   validateRequest(CategoryValidation.create),
   CategoryController.insertIntoDB
 );
 
 router.patch(
   '/:id',
+  auth(ENUM_USER_ROLE.ADMIN),
   validateRequest(CategoryValidation.update),
   CategoryController.updateSingleBooksFromDB
 );
-router.delete('/:id', CategoryController.deleteSingleBooksFromDB);
+router.delete(
+  '/:id',
+  auth(ENUM_USER_ROLE.ADMIN),
+  CategoryController.deleteSingleBooksFromDB
+);
 
 export const CategoryRoutes = router;
